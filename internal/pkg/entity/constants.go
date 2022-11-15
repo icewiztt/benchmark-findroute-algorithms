233612ee21d87d47a37938623d8a642586232168
Tran Quang Thanh <thanhqt2002@gmail.com>
2022-11-15 16:44:24 +0700

    Benchmark 3 version: perumatation with maxhop = 2, uniswap with maxhop=2,5

--- internal/pkg/entity/constants.go
+++ internal/pkg/entity/constants.go
@@ -1,8 +1,14 @@
 package entity
 
 const (
-	MaxHops                = 5
-	MaxPaths               = 2
-	ForLoopFromAmountInETH = 1
-	ForLoopToAmountInETH   = 100
+	// DefaultDexes excludes serum for stability
+	DefaultDexes = "raydiumv4,raydiumv5,goosefx,orca,whirlpool,saber"
+	USDC         = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
+	RAY          = "4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R"
+	WSOL         = "So11111111111111111111111111111111111111112"
+	MSOL         = "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So"
+	BTC          = "9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E"
+	StSOL        = "7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj"
 )
+
+var Tokens = []string{USDC, RAY, WSOL, MSOL, BTC, StSOL}
